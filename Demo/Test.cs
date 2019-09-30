f8daa00003c23195fc2a3fdbc48505b081f7f735
kirin <kirin.chen1001@gmail.com>
2019-09-30 22:23:16 +0800

    add Demo Code and Scene

---
+++ Demo/Test.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStomp;
+
+public class Test : MonoBehaviour {
+
+    private StompClient stompClient;
+    public InputField showMsgText,sendMsgText;
+
+
+
+    void Awake() {
+        Application.runInBackground = true;
+    }
+
+    public void connect() {
+        stompClient = new StompClientAll("ws://127.0.0.1:8080/gs-guide-websocket");
+        stompClient.StompConnect(_s => {
+            Debug.Log("Connect:"+_s);
+            stompClient.Subscribe("/topic/greetings", onMsg);
+        });
+    }
+
+    private void onMsg(string s) {
+        showMsgText.text += s + "\n\r";
+    }
+
+    public class SendData {
+        public string name;
+    }
+
+    public void send() {
+        SendData sd = new SendData() { name = sendMsgText.text };
+        string jStr= JsonConvert.SerializeObject(sd);
+        stompClient.SendMessage("/app/hello", jStr);
+    }
+
+}
