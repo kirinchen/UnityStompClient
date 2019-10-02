e33cbf6aac8b7e042e99c24c1a5d4743b13dfcb4
kirin <kirin.chen1001@gmail.com>
2019-10-02 14:06:36 +0800

    add NameSpace for test

--- Demo/Test.cs
+++ Demo/Test.cs
@@ -2,38 +2,39 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityStomp;
+namespace UnityStomp {
+    public class Test : MonoBehaviour {
 
-public class Test : MonoBehaviour {
-
-    private StompClient stompClient;
-    public InputField showMsgText, sendMsgText;
+        private StompClient stompClient;
+        public InputField showMsgText, sendMsgText;
 
 
 
-    void Awake() {
-        Application.runInBackground = true;
+        void Awake() {
+            Application.runInBackground = true;
+        }
+
+        public void connect() {
+            stompClient = new StompClientAll("ws://127.0.0.1:8080/gs-guide-websocket");
+            stompClient.StompConnect().add(() => {
+                Debug.Log("Connected!!!");
+                stompClient.Subscribe("/topic/greetings", onMsg);
+            });
+        }
+
+        private void onMsg(string s) {
+            showMsgText.text += s + "\n\r";
+        }
+
+        public class SendData {
+            public string name;
+        }
+
+        public void send() {
+            SendData sd = new SendData() { name = sendMsgText.text };
+            string jStr = JsonConvert.SerializeObject(sd);
+            stompClient.SendMessage("/app/hello", jStr);
+        }
+
     }
-
-    public void connect() {
-        stompClient = new StompClientAll("ws://127.0.0.1:8080/gs-guide-websocket");
-        stompClient.StompConnect().add(() => {
-            Debug.Log("Connected!!!");
-            stompClient.Subscribe("/topic/greetings", onMsg);
-        });
-    }
-
-    private void onMsg(string s) {
-        showMsgText.text += s + "\n\r";
-    }
-
-    public class SendData {
-        public string name;
-    }
-
-    public void send() {
-        SendData sd = new SendData() { name = sendMsgText.text };
-        string jStr = JsonConvert.SerializeObject(sd);
-        stompClient.SendMessage("/app/hello", jStr);
-    }
-
 }
